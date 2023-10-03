62fdafdb60965d752a1409a3eab99108573085d5
DY3413 <milosix3413@gmail.com>
2023-10-03 22:10:07 +0900

    gtaxladwifikx: Switch to SPDX License
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,9 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 DEVICE_PATH := device/samsung/gtaxladwifikx
 
 ALLOW_MISSING_DEPENDENCIES=true
--- omni_gtaxladwifikx.mk
+++ omni_gtaxladwifikx.mk
@@ -1,3 +1,9 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
