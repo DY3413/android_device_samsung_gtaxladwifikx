20162d81ca1f7b6fa03536476f8f43e7fe0b8f65
DY3413 <milosix3413@gmail.com>
2023-10-03 21:55:39 +0900

    Initialize device folder
    Signed-off-by: DY3413 <milosix3413@gmail.com>

---
+++ omni_gtaxladwifikx.mk
@@ -0,0 +1,12 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier.
+PRODUCT_NAME := omni_gtaxladwifikx
+PRODUCT_DEVICE := gtaxladwifikx
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := Samsung Galaxy Tab Advanced 2
